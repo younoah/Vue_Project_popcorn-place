9e5ae99e6f17a2bb8356cdc1999440299b964adf
minsu-zip <alstn4814@gmail.com>
2021-10-06 03:14:13 +0900

    feat : 검색 입력창 추가

---
+++ src/components/Seacrch.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Search</h1>
+  <div>
+    <input
+      v-model="searchValue"
+      placeholder="검색할 영화 제목"
+      @keyup.enter="onSubmit" />
+    <!-- <img src="../images/main.jpg" /> -->
+    <img src="../images/search_logo.svg" />
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      searchValue : '',
+      // url : '~/images/search_logo.svg'
+    }
+  },
+  methods:{
+    onSubmit(){
+      console.log(this.searchValue)
+      this.searchValue = ''
+    }
+  }
+
+}
+</script>
