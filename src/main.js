7eb8165e08fc7ba19ed7486acc157c9ee70c9c76
minsu-zip <alstn4814@gmail.com>
2021-10-06 16:57:14 +0900

    build : Vue3_bootstrap 환경 설정

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from '~/App'
 import store from '~/store'
 import router from '~/routes'
 
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
 const app = Vue.createApp(App)
 app.use(store)
 app.use(router)
+app.use(BootstrapVue3)
 app.mount('#app')
