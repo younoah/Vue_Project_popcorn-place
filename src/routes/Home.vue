b4584ea0d09540aae06f7b95d2665b2be7137630
minsu-zip <alstn4814@gmail.com>
2021-10-08 01:56:07 +0900

    feat : ModalComponent 추가 개발

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,10 +1,10 @@
 <template>
-  <Seacrch />
+  <Seacrch @cnt="cnt" />
 
   <section>
     <div class="container">
       <div
-        v-if="movieList"
+        v-if="movieList.length > 0"
         class="row">
         <div
           v-for="movie in movieList"
@@ -12,8 +12,8 @@
           class="col-12 col-sm-6 col-md-4 col-xl-3">
           <!-- 카드 영역 시작 -->
           <div
-            class="card mb-3 "
-            style="max-width: 540px;">
+            class="card mb-3"
+            style="max-width: 540px">
             <div class="row g-0 item">
               <div class="col-md-4">
                 <img
@@ -27,16 +27,30 @@
                     {{ movie.Title }}
                   </div>
                   <p class="card-text">
-                    <small class="text-muted">{{ movie.Type }} - {{ movie.Year }}</small>
+                    <small
+                      class="text-muted">{{ movie.Type }} - {{ movie.Year }}</small>
                   </p>
+                  <div>
+                    <Modal :id="movie.imdbID">
+                      <template #activator>
+                        <button
+                          type="button"
+                          class="btn btn-outline-secondary">
+                          more
+                        </button>
+                      </template>
+                    </Modal>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
           <!-- 카드 영역 끝 -->
         </div>
+        <!-- <button @click="next">
+          more
+        </button> -->
       </div>
-
       <div v-else>
         검색된 자료가 없습니다.
       </div>
@@ -45,13 +59,18 @@
 </template>
 
 <script>
-import Seacrch from  '~/components/Seacrch'
+import Seacrch from '~/components/Seacrch'
+import Modal from '~/components/Modal'
 export default {
-  components:{
-    Seacrch
+  components: {
+    Seacrch,
+    Modal,
   },
   data() {
-    return {}
+    return {
+      cnt: 1,
+      checkList: false
+    }
   },
   computed: {
     movieList() {
@@ -59,18 +78,13 @@ export default {
     },
   },
   created() {
-    this.$store.dispatch('fetchApi/getMovie', 'frozen')
+    this.$store.dispatch('fetchApi/getMovie', { title: 'frozen' })
   },
-  methods: {
-    test(){
-      console.log(this.movieList)
-    }
-  }
 }
 </script>
 
 <style lang="scss" scoped>
-.card-title{
+.card-title {
   font-size: 18px;
   overflow: hidden;
   text-overflow: ellipsis;
