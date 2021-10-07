52e6cdee78d2c8ba78c8ea198d66d5cd8bc6d923
minsu-zip <alstn4814@gmail.com>
2021-10-07 17:21:54 +0900

    feat : detail api 요청

--- src/store/fetchApi.js
+++ src/store/fetchApi.js
@@ -3,26 +3,40 @@ export default {
   state() {
     return {
       movieList: [],
+      movieDetail: ''
     }
   },
   getters: {},
   mutations: {
-    assignState(state, payload) {
+    movieListState(state, payload) {
       Object.keys(payload).forEach((key) => {
         state[key] = payload[key]
       })
     },
+    movieDetailState(state, payload){
+      console.log(payload)
+      state.movieDetail = payload.getDetail
+    }
   },
   actions: {
     async getMovie({ commit }, title = '') {
-      
       const getMovie = await fetch(
         `https://www.omdbapi.com?apikey=${VUE_APP_API_KEY}&s=${title}&page=3`
       ).then((res) => res.json())
 
       const movieList = getMovie.Search
-      commit('assignState', {
+      commit('movieListState', {
         movieList,
+      })
+    },
+
+    async getDetail({ commit }, id = '') {
+      const getDetail = await fetch(
+        `https://www.omdbapi.com?apikey=${VUE_APP_API_KEY}&i=${id}&plot=full`
+      ).then((res) => res.json())
+
+      commit('movieDetailState', {
+        getDetail,
       })
     },
   },
