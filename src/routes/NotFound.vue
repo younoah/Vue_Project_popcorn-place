45696e87738713e572d4ec6645b2d7d35822eea9
minsu-zip <alstn4814@gmail.com>
2021-10-08 16:42:08 +0900

    feat : 404page 개발

---
+++ src/routes/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="alert alert-danger"
+    role="alert">
+    NotFound. 정상적인 경로가 아닙니다. 
+    <a
+      href="#"
+      class="alert-link"> <RouterLink to="/">
+        Home 
+      </RouterLink></a>
+  </div>
+</template>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './Home'
+import NotFound from './NotFound'
 export default createRouter({
   history: createWebHistory(),
   scrollBehavior: () => ({ top: 0 }),
@@ -7,6 +8,10 @@ export default createRouter({
     {
       path: '/',
       component: Home,
+    },
+    {
+      path: '/:notFount(.*)',
+      component: NotFound,
     },
   ],
 })
