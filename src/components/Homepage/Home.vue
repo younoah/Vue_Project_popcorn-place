949bb86a270955594851564aa906b557e64bf7bc
Sunjae95 <tjswo9502@gmail.com>
2021-10-09 04:30:46 +0900

    Add: MovieList

--- src/components/Homepage/Home.vue
+++ src/components/Homepage/Home.vue
@@ -2,26 +2,54 @@
   <SearchBar
     v-model="title"
     @search="onSearch" />
+  <MovieList
+    :movies="movies" />
 </template>
 
 <script>
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 export default {
-    components: { SearchBar },
+    components: { SearchBar, MovieList },
     data() {
         return {
             title: '',
             movies: [],
-            totalCount: 0,
             page: 1,
             isLoading: false
         };
     },
+    created() {
+      // Infinity Scroll
+      // debounce intersection Observer 적용 
+      window.addEventListener('scroll', async ()=>{
+        const isScrollEnded = window.innerHeight + window.scrollY >= document.body.offsetHeight;
+
+        if(isScrollEnded && !this.isLoading) {
+          this.isLoading = true;
+          this.page++;
+        
+          const options = {title: this.title, page: this.page};
+          const {Search: searchResults} = await this.$getMovies(options);
+          
+          this.movies = [...this.movies, ...searchResults];
+          this.isLoading = false;
+        }
+      });
+    },
     methods: {
         async onSearch() {
+            if(this.isLoading) return; 
+            
+            this.isLoading = true;
+            this.page = 1;
+            
             const options = {title: this.title, page: this.page};
-            console.log(await this.$getMovies(options));
+            const {Search: searchResults} = await this.$getMovies(options);
+            
+            this.movies = searchResults;
+            this.isLoading = false;
         },
     }
 };
---
+++ src/components/Homepage/MovieList.vue
@@ -0,0 +1,93 @@
+<template>
+  <ul>
+    <li
+      v-for="movie in movies"
+      :key="movie.imdbID"
+      :class="movie.imdbID"
+      @click="click">
+      <img
+        :src="movie.Poster"
+        :alt="movie.Title" />
+      <div class="movie-info">
+        <h2>{{ movie.Title }}</h2>
+        <p>작품 년도: {{ movie.Year }}</p>
+        <p>영화 정보 보기</p>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+    props:{
+        movies: {
+            type: Array,
+            default: () => ([])
+        }
+    },
+    methods: {
+        click(key) {
+            console.log(this);
+            console.log(key);
+        }
+    }
+};
+</script>
+
+<style scoped lang="scss">
+ul {
+    display: grid;
+    grid-template-columns: repeat(2, 350px);
+    grid-auto-rows: 450px;
+    justify-content: center;
+    grid-gap: 10px;
+    
+    margin: 0px;
+    padding-top: 10px;
+}
+
+li {
+    position: relative;
+    cursor: pointer;
+    list-style: none;
+    
+    img {
+        width: 100%;
+        height: 100%;
+    }
+    
+    .movie-info {
+        position: absolute;
+        left: 0;
+        top: 0;
+
+        display: none;
+        width: 100%;
+        height: 100%;
+
+        background-color: rgba(0,0,0,0.7);
+        color: white;
+    }
+
+    &:hover{
+        .movie-info{
+            display: flex;
+            flex-direction: column;  
+            justify-content: space-evenly;
+            align-items: center;
+            
+            box-sizing: border-box;
+
+            h2 {
+                margin-top: 50px;
+                color: yellow;
+                text-align: center;
+            }
+
+            p {
+                text-align: center;
+            }
+        }
+    }
+}
+</style>
