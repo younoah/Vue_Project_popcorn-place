e24a57aae2ed8027ebfb3b3a24b177a48eb6daa9
Baek2back <dumbuk12@naver.com>
2021-10-10 01:46:11 +0900

    ✨ intersection 타겟에 위치할 ScrollDown 컴포넌트 구현

---
+++ src/components/common/ScrollDown.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="scroll-down">
+    <svg height="30" width="10">
+      <circle class="scroll-down-p1" cx="5" cy="15" r="2" />
+      <circle class="scroll-down-p1" cx="5" cy="15" r="2" />
+    </svg>
+  </div>
+</template>
+<style lang="scss" scoped>
+.scroll-down {
+  width: 30px;
+  height: 46px;
+  margin: 8px auto;
+  text-align: center;
+  border: 2px solid variables.$brand-light;
+  border-radius: 30px;
+
+  &-p1,
+  &-p2 {
+    animation-name: scrolldown;
+    animation-duration: 1.5s;
+    animation-iteration-count: infinite;
+    fill: variables.$brand-light;
+  }
+
+  &-p2 {
+    animation-delay: 0.75s;
+  }
+}
+
+@keyframes scrolldown {
+  0% {
+    transform: translate(0, -0.8rem);
+    opacity: 0;
+  }
+
+  50% {
+    transform: translate(0, 0);
+    opacity: 1;
+  }
+
+  100% {
+    transform: translate(0, 0.8rem);
+    opacity: 0;
+  }
+}
+</style>
