52c79945684f00b80ed360c661f00b3d3671484c
Baek2back <dumbuk12@naver.com>
2021-10-10 01:11:47 +0900

    ✨ BaseLayout 정의

---
+++ src/layout/BaseLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header">
+    <slot name="header" />
+  </header>
+  <main class="main">
+    <slot name="main" />
+  </main>
+</template>
+<style lang="scss" scoped>
+.header,
+.main {
+  display: flex;
+  justify-content: center;
+  max-width: 80%;
+  margin: 1.6rem auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="home"></div>
+  <BaseLayout>
+    <template v-slot:header> </template>
+    <template v-slot:main> </template>
+  </BaseLayout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import BaseLayout from "@/layout/BaseLayout.vue";
+
+const COMPONENT_NAME = "Home";
 
 export default defineComponent({
-  name: "Home",
-  components: {},
+  name: COMPONENT_NAME,
+  components: {
+    BaseLayout,
+  },
 });
 </script>
