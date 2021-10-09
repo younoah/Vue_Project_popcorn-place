024aaf5636e1c95fea9de39c2196218792a926a6
Jeongs <kimm.jeongs@gmail.com>
2021-10-09 16:20:44 +0900

    ✨ Feat: update root files

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app__wrapper">
+    <Header />
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .app__wrapper {
+    @include flexbox($ai: null);
+    flex-direction: column;
+    color: $COLOR_PRIMARY;
+    font-size: 16px;
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+
+  .content {
+    padding-top: $HEADER_HEIGHT;
+    flex-grow: 1;
+  }
+
+  
+</style>
