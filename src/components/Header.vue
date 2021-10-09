caea7e2fabf6590dea8e5b1ae48fd2d7d542b2d3
Jeongs <kimm.jeongs@gmail.com>
2021-10-09 16:22:54 +0900

    ✨ Feat: update Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,62 @@
+
+<template>
+  <header>
+    <div class="header__inner">
+      <div
+        class="logo"
+        tabindex="0"
+        aria-label="글로벌 로고"
+        @click="$router.push('/')">
+        <img
+          src="../assets/images/global-logo.png"
+          alt="글로벌 로고" />
+      </div>
+      <button class="menu-btn">
+        <span class="material-icons">
+          menu
+        </span>
+      </button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	z-index: $HEADER_INDEX;
+	background-color: rgba($COLOR_WHITE, $BASE_OPACITY);
+	backdrop-filter: blur(8px);
+}
+
+.header__inner {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		height: $HEADER_HEIGHT;
+		padding: 0 $BASE_PADDING;
+	}
+	.logo {
+		cursor: pointer;
+		img {
+			width: 120px;
+			height: 40px;
+		}
+	}
+
+	.menu-btn {
+		padding: 5px;
+		.material-icons {
+			font-size: 30px;
+			color: $COLOR_PRIMARY;
+		}
+	}
+</style>
