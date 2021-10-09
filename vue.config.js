8ad5f3a8cbee41e8d5b16670deb7ab87b4333165
Baek2back <dumbuk12@naver.com>
2021-10-10 01:05:41 +0900

    💄 font, keyframe, color 믹스인, 변수 정의

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src/"),
+      },
+    },
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+          @use "sass:color";
+          @use "sass:list";
+          @use "sass:map";
+          @use "sass:math";
+          @use "sass:meta";
+          @use "sass:selector";
+          @use "sass:string";
+          @use "@/scss/_keyframes";
+          @use "@/scss/_colors" as colors;
+          @use "@/scss/_mixins" as mixins;
+          @use "@/scss/_typography" as typography;
+        `,
+      },
+    },
+  },
+};
