a41e47f0e8cf227a87b120d196ddc5ae3d6eb100
Baek2back <dumbuk12@naver.com>
2021-10-10 01:08:39 +0900

    💄 기본 스타일 정의

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html,
+body {
+  font-size: 62.5%;
 }
 
-#nav {
-  padding: 30px;
+html {
+  box-sizing: border-box;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  height: 100vh;
+  margin: 0;
+  color: white;
+  background-color: colors.$bg-color;
 }
 </style>
