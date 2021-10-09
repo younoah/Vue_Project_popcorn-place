89c5422f3088b36cd3b869eaab0f6e5d5fadb5c4
Baek2back <dumbuk12@naver.com>
2021-10-10 01:38:52 +0900

    ✨ API가 빈 데이터를 반환했을 때를 처리할 컴포넌트 구현

---
+++ src/components/common/NotFoundMessage.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="not-found-message">{{ message }}</p>
+</template>
+<style lang="scss" scoped>
+.not-found-message {
+  @include typography.font-style(14);
+}
+</style>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+const COMPONENT_NAME = "NotFoundMessage";
+
+export default defineComponent({
+  name: COMPONENT_NAME,
+  props: {
+    message: { type: String, required: true },
+  },
+});
+</script>
