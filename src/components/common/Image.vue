8884ed5b506a707e0fe0c0c35d0dbed5902c41d9
Baek2back <dumbuk12@naver.com>
2021-10-10 01:23:16 +0900

    ✨ 영화 포스터 로딩 시 에러 발생 및 로딩 완료 감지 가능한 Image 컴포넌트 구현

---
+++ src/components/common/Image.vue
@@ -0,0 +1,40 @@
+<template>
+  <img
+    class="image"
+    :src="currentSrc"
+    :alt="alt"
+    @load="onLoad"
+    @error="onError"
+  />
+</template>
+<style lang="scss" scoped>
+.image {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+</style>
+<script lang="ts">
+import { defineComponent, PropType, ref } from "vue";
+import fallbackImg from "@/assets/logo.png";
+
+const COMPONENT_NAME = "Image";
+
+export default defineComponent({
+  name: COMPONENT_NAME,
+  props: {
+    onLoad: { type: Function as PropType<() => void>, required: true },
+    src: { type: String, required: true },
+    alt: { type: String, required: true },
+  },
+  setup(props) {
+    const currentSrc = ref<string>(props.src);
+
+    const onError = () => {
+      currentSrc.value = fallbackImg;
+    };
+
+    return { currentSrc, onError };
+  },
+});
+</script>
