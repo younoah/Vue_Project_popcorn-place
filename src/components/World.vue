787c3c6669c33281dda45d7cb2dd56fb04aa59eb
younoah <kr_uno_10@naver.com>
2021-10-11 13:17:20 +0900

    init: 뷰 프로젝트 템플릿 셋업

---
+++ src/components/World.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>World.vue</h1>
+  <div>{{ reversedMessage }}</div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapMutations, mapActions } from 'vuex';
+
+export default {
+  computed: {
+    // 일반적인 방식
+    // reversedMessage() {
+    //   return this.$store.getters['message/reversedMessage'];
+    // },
+
+    // vuex의 map 함수 방식
+    ...mapGetters('message', ['reversedMessage']),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: royalblue;
+}
+</style>
